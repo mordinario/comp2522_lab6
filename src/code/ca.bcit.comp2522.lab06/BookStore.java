2c4f39f8a7c372b3dcc8272e181f7a030fb27ba5
Marcy Ordinario <aimlesscontrivance@gmail.com>
2025-02-14 14:08:47 -0800

    Modified BookStore.java to include any T extends Literature. Javadoc yet to be modified.

--- src/code/ca.bcit.comp2522.lab06/BookStore.java
+++ src/code/ca.bcit.comp2522.lab06/BookStore.java
@@ -41,14 +41,14 @@ import java.util.*;
  * @author Marcy Ordinario
  * @version 1.0
  */
-public class BookStore
+public class BookStore<T extends Literature>
 {
     private static final int DECADE_DIVIDER = 10;
     private static final int PERCENTAGE_MULT = 100;
 
     private final String name;
-    private final List<Novel> novels;
-    private final Map<String, Novel> novelsMap;
+    private final List<T> bookList;
+    private final Map<String, T> bookMap;
 
     /**
      * Constructs a new {@code BookStore} with the specified name.
@@ -77,50 +77,16 @@ public class BookStore
         validateName(nameToBe);
         this.name = nameToBe;
 
-        novels = new ArrayList<>();
-        populateBooks();
+        bookList = new ArrayList<>();
 
         // Initializes HashMap
-        novelsMap = new HashMap<String, Novel>();
+        bookMap = new HashMap<String, T>();
 
-        for (final Novel novel : novels)
+        for (final T book : bookList)
         {
             // Gets key from each novel
-            final String titleKey = novel.getTitle();
-            novelsMap.put(titleKey, novel);
-        }
-
-        // Initializes key set and iterator
-        final Set<String>      titleSet;
-        final List<String>     titleList;
-        final Iterator<String> titleIter;
-
-        titleSet  = novelsMap.keySet();
-        titleIter = novelsMap.keySet().iterator();
-
-        // Removing any novel with "the" in the title from the map
-        while (titleIter.hasNext())
-        {
-            final String key;
-            key = titleIter.next();
-            System.out.println(key);
-
-            if (key.toLowerCase().contains("the"))
-            {
-                titleIter.remove();
-            }
-        }
-
-        // Convert the key set to an ArrayList
-        titleList = new ArrayList<>(titleSet);
-
-        // Sort the list then print using toString()
-        Collections.sort(titleList);
-        for (String key : titleList)
-        {
-            final Novel novelToPrint;
-            novelToPrint = novelsMap.get(key);
-            System.out.println(novelToPrint.toString());
+            final String titleKey = book.getTitle();
+            bookMap.put(titleKey, book);
         }
     }
 
@@ -150,116 +116,6 @@ public class BookStore
         store = new BookStore("The Bookstore");
     }
 
-    /*
-     * Populates this BookStore with Novels.
-     * All Novel data is hard-coded into this application; no
-     * functionality exists for users to add or remove Novels
-     * in this BookStore.
-     */
-    private void populateBooks()
-    {
-        novels.add(new Novel("The Adventures of Augie March", "Saul Bellow", 1953));
-        novels.add(new Novel("All the King’s Men", "Robert Penn Warren", 1946));
-        novels.add(new Novel("American Pastoral", "Philip Roth", 1997));
-        novels.add(new Novel("An American Tragedy", "Theodore Dreiser", 1925));
-        novels.add(new Novel("Animal Farm", "George Orwell", 1946));
-        novels.add(new Novel("Appointment in Samarra", "John O'Hara", 1934));
-        novels.add(new Novel("Are You There God? It’s Me, Margaret.", "Judy Blume", 1970));
-        novels.add(new Novel("The Assistant", "Bernard Malamud", 1957));
-        novels.add(new Novel("At Swim-Two-Birds", "Flann O’Brien", 1938));
-        novels.add(new Novel("Atonement", "Ian McEwan", 2002));
-        novels.add(new Novel("Beloved", "Toni Morrison", 1987));
-        novels.add(new Novel("The Berlin Stories", "Christopher Isherwood", 1946));
-        novels.add(new Novel("The Big Sleep", "Raymond Chandler", 1939));
-        novels.add(new Novel("The Blind Assassin", "Margaret Atwood", 2000));
-        novels.add(new Novel("Blood Meridian", "Cormac McCarthy", 1986));
-        novels.add(new Novel("Brideshead Revisited", "Evelyn Waugh", 1946));
-        novels.add(new Novel("The Bridge of San Luis Rey", "Thornton Wilder", 1927));
-        novels.add(new Novel("Call It Sleep", "Henry Roth", 1935));
-        novels.add(new Novel("Catch-22", "Joseph Heller", 1961));
-        novels.add(new Novel("The Catcher in the Rye", "J.D. Salinger", 1951));
-        novels.add(new Novel("A Clockwork Orange", "Anthony Burgess", 1963));
-        novels.add(new Novel("The Confessions of Nat Turner", "William Styron", 1967));
-        novels.add(new Novel("The Corrections", "Jonathan Franzen", 2001));
-        novels.add(new Novel("The Crying of Lot 49", "Thomas Pynchon", 1966));
-        novels.add(new Novel("A Dance to the Music of Time", "Anthony Powell", 1951));
-        novels.add(new Novel("The Day of the Locust", "Nathanael West", 1939));
-        novels.add(new Novel("Death Comes for the Archbishop", "Willa Cather", 1927));
-        novels.add(new Novel("A Death in the Family", "James Agee", 1958));
-        novels.add(new Novel("The Death of the Heart", "Elizabeth Bowen", 1958));
-        novels.add(new Novel("Deliverance", "James Dickey", 1970));
-        novels.add(new Novel("Dog Soldiers", "Robert Stone", 1974));
-        novels.add(new Novel("Falconer", "John Cheever", 1977));
-        novels.add(new Novel("The French Lieutenant's Woman", "John Fowles", 1969));
-        novels.add(new Novel("The Golden Notebook", "Doris Lessing", 1962));
-        novels.add(new Novel("Go Tell It on the Mountain", "James Baldwin", 1953));
-        novels.add(new Novel("Gone with the Wind", "Margaret Mitchell", 1936));
-        novels.add(new Novel("The Grapes of Wrath", "John Steinbeck", 1939));
-        novels.add(new Novel("Gravity's Rainbow", "Thomas Pynchon", 1973));
-        novels.add(new Novel("The Great Gatsby", "F. Scott Fitzgerald", 1925));
-        novels.add(new Novel("A Handful of Dust", "Evelyn Waugh", 1934));
-        novels.add(new Novel("The Heart Is a Lonely Hunter", "Carson McCullers", 1940));
-        novels.add(new Novel("The Heart of the Matter", "Graham Greene", 1948));
-        novels.add(new Novel("Herzog", "Saul Bellow", 1964));
-        novels.add(new Novel("Housekeeping", "Marilynne Robinson", 1981));
-        novels.add(new Novel("A House for Mr. Biswas", "V.S. Naipaul", 1962));
-        novels.add(new Novel("I, Claudius", "Robert Graves", 1934));
-        novels.add(new Novel("Infinite Jest", "David Foster Wallace", 1996));
-        novels.add(new Novel("Invisible Man", "Ralph Ellison", 1952));
-        novels.add(new Novel("Light in August", "William Faulkner", 1932));
-        novels.add(new Novel("The Lion, the Witch and the Wardrobe", "C.S. Lewis", 1950));
-        novels.add(new Novel("Lolita", "Vladimir Nabokov", 1955));
-        novels.add(new Novel("Lord of the Flies", "William Golding", 1954));
-        novels.add(new Novel("Snow Crash", "Neal Stephenson", 1992));
-        novels.add(new Novel("The Sot-Weed Factor", "John Barth", 1960));
-        novels.add(new Novel("The Sound and the Fury", "William Faulkner", 1929));
-        novels.add(new Novel("The Sportswriter", "Richard Ford", 1986));
-        novels.add(new Novel("The Spy Who Came in from the Cold", "John le Carré", 1964));
-        novels.add(new Novel("The Sun Also Rises", "Ernest Hemingway", 1926));
-        novels.add(new Novel("Their Eyes Were Watching God", "Zora Neale Hurston", 1937));
-        novels.add(new Novel("Things Fall Apart", "Chinua Achebe", 1959));
-        novels.add(new Novel("To Kill a Mockingbird", "Harper Lee", 1960));
-        novels.add(new Novel("To the Lighthouse", "Virginia Woolf", 1929));
-        novels.add(new Novel("Tropic of Cancer", "Henry Miller", 1934));
-        novels.add(new Novel("Ubik", "Philip K. Dick", 1969));
-        novels.add(new Novel("Under the Net", "Iris Murdoch", 1954));
-        novels.add(new Novel("Under the Volcano", "Malcolm Lowry", 1947));
-        novels.add(new Novel("Watchmen", "Alan Moore and Dave Gibbons", 1986));
-        novels.add(new Novel("White Noise", "Don DeLillo", 1985));
-        novels.add(new Novel("White Teeth", "Zadie Smith", 2000));
-        novels.add(new Novel("Wide Sargasso Sea", "Jean Rhys", 1966));
-        novels.add(new Novel("The Lord of the Rings", "J.R.R. Tolkien", 1954));
-        novels.add(new Novel("Loving", "Henry Green", 1945));
-        novels.add(new Novel("Lucky Jim", "Kingsley Amis", 1954));
-        novels.add(new Novel("The Man Who Loved Children", "Christina Stead", 1940));
-        novels.add(new Novel("Midnight's Children", "Salman Rushdie", 1981));
-        novels.add(new Novel("Money", "Martin Amis", 1984));
-        novels.add(new Novel("The Moviegoer", "Walker Percy", 1961));
-        novels.add(new Novel("Mrs. Dalloway", "Virginia Woolf", 1925));
-        novels.add(new Novel("Naked Lunch", "William Burroughs", 1959));
-        novels.add(new Novel("Native Son", "Richard Wright", 1940));
-        novels.add(new Novel("Neuromancer", "William Gibson", 1984));
-        novels.add(new Novel("Never Let Me Go", "Kazuo Ishiguro", 2005));
-        novels.add(new Novel("1984", "George Orwell", 1948));
-        novels.add(new Novel("On the Road", "Jack Kerouac", 1957));
-        novels.add(new Novel("One Flew Over the Cuckoo's Nest", "Ken Kesey", 1962));
-        novels.add(new Novel("The Painted Bird", "Jerzy Kosinski", 1965));
-        novels.add(new Novel("Pale Fire", "Vladimir Nabokov", 1962));
-        novels.add(new Novel("A Passage to India", "E.M. Forster", 1924));
-        novels.add(new Novel("Play It as It Lays", "Joan Didion", 1970));
-        novels.add(new Novel("Portnoy's Complaint", "Philip Roth", 1969));
-        novels.add(new Novel("Possession", "A.S. Byatt", 1990));
-        novels.add(new Novel("The Power and the Glory", "Graham Greene", 1939));
-        novels.add(new Novel("The Prime of Miss Jean Brodie", "Muriel Spark", 1961));
-        novels.add(new Novel("Rabbit, Run", "John Updike", 1960));
-        novels.add(new Novel("Ragtime", "E.L. Doctorow", 1975));
-        novels.add(new Novel("The Recognitions", "William Gaddis", 1955));
-        novels.add(new Novel("Red Harvest", "Dashiell Hammett", 1929));
-        novels.add(new Novel("Revolutionary Road", "Richard Yates", 1961));
-        novels.add(new Novel("The Sheltering Sky", "Paul Bowles", 1949));
-        novels.add(new Novel("Slaughterhouse-Five", "Kurt Vonnegut", 1969));
-    }
-
     /**
      * Prints all the novel titles in this bookstore in uppercase.
      *
@@ -269,10 +125,10 @@ public class BookStore
      */
     public void printAllTitles()
     {
-        for (final Novel novel : novels)
+        for (final T book : bookList)
         {
             final String upperCaseNovel;
-            upperCaseNovel = novel.getTitle().toUpperCase();
+            upperCaseNovel = book.getTitle().toUpperCase();
             System.out.println(upperCaseNovel);
         }
     }
@@ -292,14 +148,14 @@ public class BookStore
         final String stringToCompare;
         stringToCompare = substring.toLowerCase();
 
-        for (final Novel novel : novels)
+        for (final T book : bookList)
         {
             final String titleToCompare;
-            titleToCompare = novel.getTitle().toLowerCase();
+            titleToCompare = book.getTitle().toLowerCase();
 
             if (titleToCompare.contains(stringToCompare))
             {
-                System.out.println(novel.getTitle());
+                System.out.println(book.getTitle());
             }
         }
     }
@@ -314,14 +170,14 @@ public class BookStore
      */
     public void printTitlesInAlphaOrder()
     {
-        final List<Novel> sortedNovels;
-        sortedNovels = novels;
+        final List<T> sortedBooks;
+        sortedBooks = bookList;
 
-        Collections.sort(sortedNovels);
+        Collections.sort(sortedBooks);
 
-        for (final Novel alphaNovel : sortedNovels)
+        for (final T alphaBook : sortedBooks)
         {
-            System.out.println(alphaNovel.getTitle());
+            System.out.println(alphaBook.getTitle());
         }
     }
 
@@ -338,17 +194,17 @@ public class BookStore
      */
     public void printGroupByDecade(final int decadeToPrint)
     {
-        for (final Novel novel : novels)
+        for (final T book : bookList)
         {
             final int yearPublished;
             final int decadeToCompareTo;
 
-            yearPublished = novel.getYearPublished() / DECADE_DIVIDER;
+            yearPublished = book.getYearPublished() / DECADE_DIVIDER;
             decadeToCompareTo = decadeToPrint / DECADE_DIVIDER;
 
             if (yearPublished == decadeToCompareTo)
             {
-                System.out.println(novel.getTitle());
+                System.out.println(book.getTitle());
             }
         }
     }
@@ -364,25 +220,25 @@ public class BookStore
      */
     public void getLongest()
     {
-        Novel novelWithLongestTitle;
+        T bookWithLongestTitle;
         // MAYBE consider the edge case where the
         // book store is empty
-        novelWithLongestTitle = novels.getFirst();
+        bookWithLongestTitle = bookList.getFirst();
 
-        for (final Novel novelToCompare : novels)
+        for (final T bookToCompare : bookList)
         {
             final int longestTitleLength;
             final int comparedTitleLength;
 
-            longestTitleLength = novelWithLongestTitle.getTitle().length();
-            comparedTitleLength = novelToCompare.getTitle().length();
+            longestTitleLength = bookWithLongestTitle.getTitle().length();
+            comparedTitleLength = bookToCompare.getTitle().length();
             if (comparedTitleLength > longestTitleLength)
             {
-                novelWithLongestTitle = novelToCompare;
+                bookWithLongestTitle = bookToCompare;
             }
         }
 
-        System.out.println(novelWithLongestTitle.getTitle());
+        System.out.println(bookWithLongestTitle.getTitle());
     }
 
     /**
@@ -399,12 +255,12 @@ public class BookStore
      */
     public boolean isThereABookWrittenBetween(final int yearToSearch)
     {
-        for (final Novel novel : novels)
+        for (final T book : bookList)
         {
-            final int novelPublicationYear;
-            novelPublicationYear = novel.getYearPublished();
+            final int bookPublicationYear;
+            bookPublicationYear = book.getYearPublished();
 
-            if (novelPublicationYear == yearToSearch)
+            if (bookPublicationYear == yearToSearch)
             {
                 return true;
             }
@@ -430,11 +286,12 @@ public class BookStore
         int counter;
         counter = 0;
 
-        for (final Novel novel : novels)
+        for (final T book : bookList)
         {
             final String substringToCompare;
             final String titleToCompare;
-            titleToCompare = novel.getTitle().toLowerCase();
+
+            titleToCompare = book.getTitle().toLowerCase();
             substringToCompare = substring.toLowerCase();
 
             if (titleToCompare.contains(substringToCompare))
@@ -466,26 +323,26 @@ public class BookStore
     public int whichPercentWrittenBetween(final int lowerYearBound,
                                           final int upperYearBound)
     {
-        final int novelPercentage;
-        final int totalNovels;
+        final int bookPercentage;
+        final int totalBooks;
 
-        int novelsWrittenBetweenYears;
+        int booksWrittenBetweenYears;
 
-        novelsWrittenBetweenYears = 0;
-        totalNovels = novels.size();
+        booksWrittenBetweenYears = 0;
+        totalBooks = bookList.size();
 
-        for (final Novel novel : novels)
+        for (final T book : bookList)
         {
-            if (novel.getYearPublished() >= lowerYearBound
-                && novel.getYearPublished() <= upperYearBound)
+            if (book.getYearPublished() >= lowerYearBound
+                && book.getYearPublished() <= upperYearBound)
             {
-                novelsWrittenBetweenYears++;
+                booksWrittenBetweenYears++;
             }
         }
 
-        novelPercentage = (novelsWrittenBetweenYears * PERCENTAGE_MULT)
-                          / totalNovels;
-        return novelPercentage;
+        bookPercentage = (booksWrittenBetweenYears * PERCENTAGE_MULT)
+                          / totalBooks;
+        return bookPercentage;
     }
 
     /**
@@ -499,26 +356,26 @@ public class BookStore
      * @return the {@link Novel} object representing the oldest novel
      *         in the bookstore
      */
-    public Novel getOldestBook()
+    public T getOldestBook()
     {
-        Novel oldestNovel;
-        oldestNovel = novels.getFirst();
+        T oldestBook;
+        oldestBook = bookList.getFirst();
 
-        for (final Novel novel : novels)
+        for (final T book : bookList)
         {
             final int oldestPublicationYear;
             final int yearToCompare;
 
-            oldestPublicationYear = oldestNovel.getYearPublished();
-            yearToCompare = novel.getYearPublished();
+            oldestPublicationYear = oldestBook.getYearPublished();
+            yearToCompare = book.getYearPublished();
 
             if (yearToCompare < oldestPublicationYear)
             {
-                oldestNovel = novel;
+                oldestBook = book;
             }
         }
 
-        return oldestNovel;
+        return oldestBook;
     }
 
     /**
@@ -533,16 +390,16 @@ public class BookStore
      * @return an {@code ArrayList} of {@link Novel} objects that have
      *         titles with the specified length
      */
-    public List<Novel> getBooksThisLength(final int titleLength)
+    public List<T> getBooksThisLength(final int titleLength)
     {
-        final List<Novel> booksThisLength;
-        booksThisLength = new ArrayList<Novel>();
+        final List<T> booksThisLength;
+        booksThisLength = new ArrayList<T>();
 
-        for (final Novel novel : novels)
+        for (final T book : bookList)
         {
-            if (novel.getTitle().length() == titleLength)
+            if (book.getTitle().length() == titleLength)
             {
-                booksThisLength.add(novel);
+                booksThisLength.add(book);
             }
         }
 
